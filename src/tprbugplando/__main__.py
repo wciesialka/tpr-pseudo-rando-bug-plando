1b3cae49bfadfdfbd444539a570e4fb8d42eb993
Willow Ciesialka <wciesialka@gmail.com>
2024-09-14 01:01:20 -0500

    Pairings now output alphabetically

--- src/tprbugplando/__main__.py
+++ src/tprbugplando/__main__.py
@@ -2,7 +2,7 @@ from . import items
 from . import rungen
 def main():
     pairings = rungen.generate_pairings(items.PROGRESSION_ITEMS)
-    for check, item in pairings.items():
+    for check, item in sorted(pairings.items()):
         print(f"\033[93m{check}\033[0m: \033[92m{item}\033[0m")
 
 if __name__ == "__main__":
